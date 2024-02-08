f3e9f8dedf527ef442a6963aba72244610ffae76
Skye J <skye@skyenet.online>
2024-02-08 09:45:15 -0500

    Removing obsolete code and changing search engine
    Obsolete code was the previous style sheet switch, and the search engine was changed from
    ecosia to duckduckgo

--- run.js
+++ run.js
@@ -1,14 +1,3 @@
-var cook = document.cookie;
-console.log(cook);
-if (cook == ""){
-  setStyleSheetLight();
-}
-else if (cook =="style=dark"){
-  setStyleSheetDark();
-}
-else if (cook == "style=light"){
-  setStyleSheetLight();
-}
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -17,4 +6,3 @@ function myFunction() {
     x.className = "topnav";
   }
 }
-
